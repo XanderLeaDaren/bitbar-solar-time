005a837d97ad2a1dd59d59feedac6d53fe2d280f
Rommel MARTINEZ <ebzzry@ebzzry.io>
2019-03-09 15:15:33 +0800

    Use json library

--- solar-time.1s.py
+++ solar-time.1s.py
@@ -13,23 +13,31 @@
 import datetime
 from math import sin
 import time
+import json
+import urllib
 
-# Local time and day of the year
-today = datetime.datetime.now()
-day = today.timetuple().tm_yday
+def longitude():
+    url = 'http://ipinfo.io/json'
+    response = urllib.urlopen(url)
+    data = response.read()
+    return json.loads(data.decode('utf-8'))["loc"].split(",")[1]
 
-# Longitude on Earth (easily find it on http://ipinfo.io/json)
-lg = 5.1413
-pos = lg/360*24*60
+def print_information():
+    today = datetime.datetime.now()
+    day = today.timetuple().tm_yday
+    tz = time.timezone
 
-# Time zone and Equation of time
-tz = time.timezone
-eq_time = 7.655*sin(2*(day-4))+9.873*sin(4*(day-172))
+    lg = float(longitude())
+    pos = lg / 360 * 24 * 60
+    eq_time = 7.655 * sin(2 * (day - 4)) + 9.873 * sin(4 * (day - 172))
 
-# Local solar time
-sun = today - datetime.timedelta(minutes=-pos+eq_time,seconds=-tz)
-print "☀️ "+sun.strftime('%H:%M:%S')
-print "---"
-print "Time Zone Offset: "+str(tz/3600)+" h"
-print "Position Offset: %.3f" % pos+" min"
-print "Equation of Time: %.3f" % -eq_time+" min"
+    sun = today - datetime.timedelta(minutes = -pos + eq_time, seconds = -tz)
+
+    print "☀️ " + sun.strftime('%H:%M:%S')
+    print "---"
+    print "Time Zone Offset: " + str(tz / 3600) + " h"
+    print "Position Offset: %.3f" % pos + " min"
+    print "Equation of Time: %.3f" % -eq_time + " min"
+
+if __name__ == "__main__":
+    print_information()
